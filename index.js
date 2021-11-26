d08a6ea334de0da96091261cca1cf644637b8577
abaagu10 <76157233+abaagu10@users.noreply.github.com>
2021-11-26 15:27:49 -0800

    recent addition
    css, javaScript and Html updates

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 let mainContainer=document.querySelector(".main");
 var side=document.querySelector(".sidebar");
 var leftContainer =document.querySelector(".sidebar-left"); 
-let wholePage = [side,mainContainer,leftContainer];
 var blue = document.querySelector(".dot1");
 var pinki = document.querySelector(".dot2");
 var green = document.querySelector(".dot3");
-var white = document.querySelector(".dot4")
-var clickPinki = 
+var white = document.querySelector(".dot4");
+var link = document.querySelector(".live-link");
+var selector = document.querySelector("select");
+let wholePage = [mainContainer,leftContainer,link,selector];
 // change background color to pink
 pinki.addEventListener('click', function changePink() {
   for (let i = 0; i < wholePage.length; i++) {
      wholePage[i].style.backgroundColor = "rgb(200, 36, 91)";
   }  
+  
 }) 
 // change backgrounf color to blue
 blue.addEventListener('click', function changeBlue() {
